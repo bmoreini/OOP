6df829ec8123d77213e8b686ba6f5b5445f07a6a
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-12-14 13:16:35 -0500

    Create package-info.java

---
+++ vehicles/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Data Abstraction, Rep Invariants and Rep OK, Static & Dynamic Polymorphism, Assignment vs Declaration Types, Interfaces
+ */
+/**
+ * @author Bram
+ *
+ */
+package edu.vtc.cis2260.vehicles;
